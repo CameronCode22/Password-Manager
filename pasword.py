cd76b510f3203bf538d0a6b60b766e9911aa223c
CameronCode22 <131026245+CameronCode22@users.noreply.github.com>
2024-02-03 21:25:04 +0000

    Encryption
    Encryption added, and finished functions add and view. With a split function on viewed data using a for loop for each line.

--- pasword.py
+++ pasword.py
@@ -1,22 +1,33 @@
-pwd = input("What is the master password?")
+from cryptography.fernet import Fernet
+
+master_pwd = input("What is the master password?")
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+write_key()
 
 def view():
-    print("Tim")
-    print("Tim")
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = print(line.rstrip())
+            user, passw = data.split("|")
+            print("User:", user, "Password:", passw)
 
 def add():
     name = input('Account Name: ')
     pwd = input("Password: ")
 
     with open('passwords.txt', 'w') as f:
-        f.write(name +"|" + pwd)
+        f.write(name + "|" + pwd + "\n")
 
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones")
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ")
     if mode == "q":
         break
-
     if mode == "view":
         view()
     elif mode == "add":
